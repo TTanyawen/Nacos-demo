80d4d59c1c38de888aabab2af0ec7d92c734d0c0
Tan Yawen <2749934612@qq.com>
2025-05-26 15:52:53 +0800

    服务注册实现

---
+++ nacos-stock/src/main/java/example/NacosStockApplication.java
@@ -0,0 +1,14 @@
+package example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosStockApplication.class, args);
+    }
+
+}
