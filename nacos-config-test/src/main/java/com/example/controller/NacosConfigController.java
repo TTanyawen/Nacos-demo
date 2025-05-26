0829caeae1a0424dc909376f71058de7714aec5b
Tan Yawen <2749934612@qq.com>
2025-05-26 15:29:02 +0800

    第一次提交

---
+++ nacos-config-test/src/main/java/com/example/controller/NacosConfigController.java
@@ -0,0 +1,49 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/nacos")
+@RefreshScope
+public class NacosConfigController {
+
+    @Value("${java1234.name}")
+    private String name;
+
+    @Value("${java1234.age}")
+    private String age;
+
+
+    @Value("${java1234.mysql.common}")
+    private String mysql;
+
+    @Value("${java1234.redis.common}")
+    private String redis;
+
+//    @Value("${java1234.crm.config}")
+//    private String crm;
+
+    @Value("${java1234.oa.config}")
+    private String oa;
+
+
+
+    @GetMapping("/getConfigInfo")
+    public String getConfigInfo(){
+        return name+":"+age;
+    }
+
+//    @GetMapping("/getCrmConfigInfo")
+//    public String getCrmConfigInfo(){
+//        return mysql+":"+redis+":"+crm;
+//    }
+
+    @GetMapping("/getOAConfigInfo")
+    public String getOAConfigInfo(){
+        return mysql+":"+redis+":"+oa;
+    }
+}
